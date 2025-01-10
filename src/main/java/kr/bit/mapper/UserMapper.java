55efe57ea27fab692495ef824ea9c226d70c27a2
kisuk <cks71714@naver.com>
2025-01-10 16:20:26 +0900

    login add

--- src/main/java/kr/bit/mapper/UserMapper.java
+++ src/main/java/kr/bit/mapper/UserMapper.java
@@ -7,5 +7,6 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
 
     @Select("select user_idx from user_table2 where user_id=#{user_id} and user_pw=#{user_pw}")
-    User getLoginUser(User loginProBean);
+    User getLoginUser(User loginProBean); // user_idx = 1
+
 }
