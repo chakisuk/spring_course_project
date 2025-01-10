b02bce003cea693cf6200657693090224f44ea71
pgw30508 <pgw30508@daum.net>
2025-01-10 13:11:52 +0900

    beans_1

---
+++ src/main/java/kr/bit/beans/Course.java
@@ -0,0 +1,32 @@
+package kr.bit.beans;
+
+public class Course {
+
+    private int course_idx;
+    private int subject_idx;
+    private int user_idx;
+
+    public int getCourse_idx() {
+        return course_idx;
+    }
+
+    public void setCourse_idx(int course_idx) {
+        this.course_idx = course_idx;
+    }
+
+    public int getSubject_idx() {
+        return subject_idx;
+    }
+
+    public void setSubject_idx(int subject_idx) {
+        this.subject_idx = subject_idx;
+    }
+
+    public int getUser_idx() {
+        return user_idx;
+    }
+
+    public void setUser_idx(int user_idx) {
+        this.user_idx = user_idx;
+    }
+}
---
+++ src/main/java/kr/bit/beans/Subject.java
@@ -0,0 +1,59 @@
+package kr.bit.beans;
+
+public class Subject {
+
+    private int subject_idx;
+    private String subject_name;
+    private int subject_grade;
+    private int subject_num;
+    private String subject_day;
+    private String subject_time;
+
+    public int getSubject_idx() {
+        return subject_idx;
+    }
+
+    public void setSubject_idx(int subject_idx) {
+        this.subject_idx = subject_idx;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+
+    public int getSubject_grade() {
+        return subject_grade;
+    }
+
+    public void setSubject_grade(int subject_grade) {
+        this.subject_grade = subject_grade;
+    }
+
+    public int getSubject_num() {
+        return subject_num;
+    }
+
+    public void setSubject_num(int subject_num) {
+        this.subject_num = subject_num;
+    }
+
+    public String getSubject_day() {
+        return subject_day;
+    }
+
+    public void setSubject_day(String subject_day) {
+        this.subject_day = subject_day;
+    }
+
+    public String getSubject_time() {
+        return subject_time;
+    }
+
+    public void setSubject_time(String subject_time) {
+        this.subject_time = subject_time;
+    }
+}
